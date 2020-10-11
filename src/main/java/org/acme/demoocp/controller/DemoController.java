aa98661c8339d171f9080964e9353fcc5af9a33a
Muhammad Khairul Habib Muttaqien <khairul.habib@ibm.com>
2020-10-11 12:08:43 +0700

    init spring-demo openshift

---
+++ src/main/java/org/acme/demoocp/controller/DemoController.java
@@ -0,0 +1,13 @@
+package org.acme.demoocp.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DemoController{
+
+    @GetMapping("/")
+    public String hello(){
+        return "Hello World, All";
+    }
+}
---
+++ src/test/java/org/acme/demoocp/DemoOcpApplicationTests.java
@@ -0,0 +1,13 @@
+package org.acme.demoocp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoOcpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
