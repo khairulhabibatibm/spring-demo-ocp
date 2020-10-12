dab2b1999eee98566e84f71164bc7cd897152eab
Muhammad Khairul Habib Muttaqien <khairul.habib@ibm.com>
2020-10-12 20:34:22 +0700

    put back spring security

--- src/main/java/org/acme/demoocp/DemoOcpApplication.java
+++ src/main/java/org/acme/demoocp/DemoOcpApplication.java
@@ -2,11 +2,11 @@ package org.acme.demoocp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-// import org.springframework.boot.autoconfigure.security.SecurityProperties;
-// import org.springframework.context.annotation.Configuration;
-// import org.springframework.core.annotation.Order;
-// import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-// import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.boot.autoconfigure.security.SecurityProperties;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @SpringBootApplication
 public class DemoOcpApplication {
@@ -15,18 +15,17 @@ public class DemoOcpApplication {
 		SpringApplication.run(DemoOcpApplication.class, args);
 	}
 
-	// @Configuration
-	// protected static class SecurityConfiguration extends WebSecurityConfigurerAdapter {
-	// 	@Override
-	// 	protected void configure(HttpSecurity http) throws Exception {
-	// 	http
-	// 		.httpBasic()
-	// 	.and()
-	// 		.authorizeRequests()
-	// 		.antMatchers("/index.html", "/", "/home", "/login","/api").permitAll();
-	// 	http.csrf().disable();
-	// 	http.cors().disable();
-	// 	}
-  	// }
+	@Configuration
+	protected static class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+		@Override
+		protected void configure(HttpSecurity http) throws Exception {
+		http
+			.httpBasic()
+		.and()
+			.authorizeRequests()
+			.antMatchers("/index.html", "/", "/home", "/login","/api").permitAll();
+		http.csrf().disable();
+		}
+  	}
 
 }
--- src/main/java/org/acme/demoocp/controller/DemoController.java
+++ src/main/java/org/acme/demoocp/controller/DemoController.java
@@ -9,9 +9,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@CrossOrigin(origins = "*", allowedHeaders = "*")
 @RestController()
 public class DemoController{
 
@@ -24,7 +24,6 @@ public class DemoController{
         return service.getAll();
     }
 
-    // @CrossOrigin(origins = "http://localhost:8082")
     @PostMapping("/demo")
     public ModelResponse add(DemoInput tutorial){
         System.out.println("in add demo");
